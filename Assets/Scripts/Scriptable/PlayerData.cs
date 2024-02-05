952d71fe3ed50b598745c9d4c785adefacfebcb9
Nix-TheMix <nwlodarczyk@mail.bradley.edu>
2024-02-05 16:43:22 -0600

    Saved data
    saved player data

--- Assets/Scripts/Scriptable/PlayerData.cs
+++ Assets/Scripts/Scriptable/PlayerData.cs
@@ -2,17 +2,9 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerData : MonoBehaviour
+[CreateAssetMenu(fileName = "PlayerData", menuName = "Player Data")]
+public class PlayerData : ScriptableObject
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [SerializeField] private string PlayerName;
+    [SerializeField] private int PlayerHealth;
 }
